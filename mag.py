db3e5697409e8e4baf88af538bcbf1f5f8b22383
rileyrgiauque <riley.r.giauque@gmail.com>
2026-03-06 15:47:00 -0500

    Update mag.py

--- mag.py
+++ mag.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, jsonify
 import requests
+import os
 
 app = Flask(__name__)
 
@@ -44,4 +45,5 @@ ZIP: {zip_code}
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
